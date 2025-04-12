bfa17dff25192ede8eeb831a909e54cd51e3a864
Talon <146667839+cnTalon@users.noreply.github.com>
2025-04-12 09:06:39 +0000

    read in lines

--- level1.py
+++ level1.py
@@ -1,2 +1,7 @@
-f = open("1.txt", "r")
-print(f.read())
+with open("1.txt", "r") as file:
+    size = file.readline().strip()
+    depot = file.readline().strip()
+    capacity = file.readline().strip()
+    storages = file.readline().strip()
+    enclosures = file.readline().strip()
+    deadzones = file.readline().strip()
