5a7eacefcd7342e6cd03a3ed8b2854bcb4de5855
Talon <146667839+cnTalon@users.noreply.github.com>
2025-04-12 09:54:32 +0000

    fixed some bugs

--- level1.py
+++ level1.py
@@ -5,16 +5,16 @@ with open("1.txt", 'r') as f:
     deadzones = []
 
     # Parse dimensions
-    size = lines[0].replace('ZOO DIMENSIONS', '').strip()[1:-1].split(",")
+    size = lines[0].strip()[1:-1].split(",")
 
     # Parse drone depot
-    depot = lines[1].replace('DRONE DEPOT', '').strip()[1:-1].split(",")
+    depot = lines[1].strip()[1:-1].split(",")
 
     # Parse battery capacity
-    capacity = int(lines[2].replace('BATTERY DISTANCE CAPACITY', '').strip()[1:-1])
+    capacity = int(lines[2].strip()[1:-1])
 
     # Parse food storages
-    storage = lines[3].replace('FOOD STORAGES COORDINATES', '').strip()[1:-1]
+    storage = lines[3].strip()[1:-1]
     if storage:
         for fs in storage.split('),'):
             clean = fs.strip()[1:].replace(')', '')
@@ -23,7 +23,7 @@ with open("1.txt", 'r') as f:
                 storages.append(fs)
 
     # Parse enclosures
-    enclosure = lines[4].replace('ENCLOSURES', '').strip()[1:-1]
+    enclosure = lines[4].strip()[1:-1]
     if enclosure:
         for enc in enclosure.split('),'):
             clean = enc.strip()[1:].replace(')', '')
@@ -32,7 +32,7 @@ with open("1.txt", 'r') as f:
                 enclosures.append(enc)
 
     # Parse deadzones
-    deadzone = lines[5].replace('DEADZONES', '').strip()[1:-1]
+    deadzone = lines[5].strip()[1:-1]
     if deadzone:
         for dz in deadzone.split('),'):
             clean = dz.strip()[1:].replace(')', '')
