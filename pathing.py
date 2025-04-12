a33fcb2983e62035cee6da6efd37e501e37fcf7f
Talon <146667839+cnTalon@users.noreply.github.com>
2025-04-12 10:51:16 +0000

    finished pathing

--- pathing.py
+++ pathing.py
@@ -1,1 +1,24 @@
 import math
+from typing import Tuple
+
+# distance formula for path calculation
+def distance (p1: Tuple[int, int], p2: Tuple[int, int]) -> float:
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+def paths(zoo):
+    paths = []
+    fed = set()
+
+    for enc in sorted(zoo.enclosures, key = lambda e: (-e[3], distance(zoo.depot[:2], e[:2]))):
+        if (enc[0], enc[1], enc[2]) in fed:     # checks if enclosure is fed
+            continue
+
+        diet = enc[4]   #gets diet and checks for nearest food source
+        nearestSource = min((fs for fs in zoo.storages if fs[3] == diet), key=lambda fs: distance(zoo.depot[:2], fs[:2]))
+
+    # adds path to list [depot, food source, enclosure, depot]
+    paths.append([zoo.depot[:2], nearestSource[:2], enc[:2], zoo.depot[:2]])
+
+    fed.add((enc[0], enc[1], enc[2]))   # adds to list of fed spots
+
+    return paths
