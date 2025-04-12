6490efc5e667ed07adffb163ef20dac32784e87a
Talon <146667839+cnTalon@users.noreply.github.com>
2025-04-12 12:27:18 +0000

    update

--- pathing.py
+++ pathing.py
@@ -5,9 +5,17 @@ from typing import Tuple
 def distance (p1: Tuple[int, int], p2: Tuple[int, int]) -> float:
     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-def paths(zoo):
+def paths(zoo, capacity):
     paths = [zoo.depot[:2]]
+    currentPath = []
+    remaining = zoo.capacity
     fed = set()
+
+    #start at depot
+    depot = zoo.depot[:2]
+    takeoffCost = 50 - zoo.depot[2]
+    currentPath.append(depot)
+    remaining -= takeoffCost
 
     # sort by diet
     for diet in ['c', 'o', 'h']:
@@ -17,16 +25,49 @@ def paths(zoo):
         if not enclosures:
             continue
 
-        nearestSource = min((fs for fs in zoo.storages if fs[3] == diet), key=lambda fs: distance(zoo.depot[:2], fs[:2]))
-        if paths[-1] != nearestSource[:2]:
-            paths.append(nearestSource[:2])
+        nearestSource = min((fs for fs in zoo.storages if fs[3] == diet), key=lambda fs: distance(currentPath[-1], fs[:2]))
+        
+        fsXY, fsZ = nearestSource[:2], nearestSource[2]
+
+        if currentPath[-1] != fsXY:
+            moveCost = (distance(currentPath[-1], fsXY) + (50 - fsZ)) * 2 #ladning + takeoff
+            if remaining < moveCost:
+                returnDepot = distance(currentPath[-1], depot) + takeoffCost
+                currentPath.append(depot)
+                paths.append(currentPath)
+                currentPath = [depot]
+                remaining = capacity - takeoffCost
+            currentPath.append(fsXY)
+            remaining -= moveCost
 
         for enc in enclosures:
-            paths.append(enc[:2])
+            encXY, encZ = enc[:2], enc[2]
+            moveCost = (distance(currentPath[-1], encXY) + (50 - encZ) *2)
+
+            if remaining < moveCost:
+                returnCost = distance(currentPath[-1], depot) + takeoffCost
+                currentPath.append(depot)
+                paths.append(currentPath)
+                currentPath = [depot]
+                remaining = capacity - takeoffCost
+
+                fsMoveCost = (distance(depot, fsXY) + (50 - fsZ) * 2)
+                currentPath.append(fsXY)
+                remaining -= fsMoveCost
+
+            currentPath.append(encXY)
+            remaining -= moveCost
             fed.add((enc[0], enc[1], enc[2]))   # adds to list of fed spots
     
-    if paths[-1] != zoo.depot[:2]:
-        paths.append(zoo.depot[:2])
+    if len(currentPath) > 1 and currentPath[-1] != depot:
+        returnCost = distance(currentPath[-1], depot) + takeoffCost
+        if remaining >= returnCost:
+            currentPath.append(depot)
+        else:
+            paths.append(currentPath)
+            currentPath = [depot, depot]
+        paths.append(currentPath)
+    
     # sorts by priority, then distance if priority is the same
     #for enc in sorted(zoo.enclosures, key = lambda e: (-e[3], distance(zoo.depot[:2], e[:2]))):
     #    if (enc[0], enc[1], enc[2]) in fed:     # checks if enclosure is fed
